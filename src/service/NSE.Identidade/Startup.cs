f11dd9d9af167646f0a55ee7ab36e9691cfa26a1
MarcioSebastiao <marcio.sx@outlook.com>
2020-12-24 12:43:54 -0300

    Swagger adicionado na api de identidade

--- src/service/NSE.Identidade/Startup.cs
+++ src/service/NSE.Identidade/Startup.cs
@@ -5,6 +5,7 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
 using NSE.Identidade.Data;
 
 namespace NSE.Identidade
@@ -29,10 +30,24 @@ namespace NSE.Identidade
                 .AddDefaultTokenProviders();
 
             services.AddControllers();
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Title = "NerdStore Enterprise Identity API"
+                });
+            });
         }
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
+            });
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
