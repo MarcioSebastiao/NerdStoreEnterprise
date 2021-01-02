80a8fac1a8aa41b560e46926437f720a2c830e63
MarcioSebastiao <marcio.sx@outlook.com>
2021-01-02 20:31:17 -0300

    Acesso a dados para a api de catalogo

--- src/service/NSE.Catalogo/Startup.cs
+++ src/service/NSE.Catalogo/Startup.cs
@@ -1,8 +1,12 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using NSE.Catalogo.Data;
+using NSE.Catalogo.Data.Repository;
+using NSE.Catalogo.Models;
 
 namespace NSE.Catalogo
 {
@@ -17,7 +21,12 @@ namespace NSE.Catalogo
 
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<CatalogoContext>(options =>
+                options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
             services.AddControllers();
+
+            services.AddScoped<IProdutoRepository, ProdutoRepository>();
+            services.AddScoped<CatalogoContext>();
         }
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
