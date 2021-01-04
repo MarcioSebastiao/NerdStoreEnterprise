0b69f38a1fbec507d09b2e3af149bf748469a01d
MarcioSebastiao <marcio.sx@outlook.com>
2021-01-04 12:17:14 -0300

    CatalogoController - Obter todos os produtos e obter produto por Id

---
+++ src/service/NSE.Catalogo/Controllers/CatalogoController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NSE.Catalogo.Models;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace NSE.Catalogo.Controllers
+{
+    [ApiController]
+    public class CatalogoController : Controller
+    {
+        private readonly IProdutoRepository _produtoRepository;
+
+        public CatalogoController(IProdutoRepository produtoRepository)
+        {
+            _produtoRepository = produtoRepository;
+        }
+
+        [HttpGet("catalogo/produtos")]
+        public async Task<IEnumerable<Produto>> Index()
+        {
+            return await _produtoRepository.ObterTodos();
+        }
+
+        [HttpGet("catalogo/produtos/{id}")]
+        public async Task<Produto> ProdutoDetalhe(Guid Id)
+        {
+            return await _produtoRepository.ObterPorId(Id);
+        }
+    }
+}
