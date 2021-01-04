ca563eba54319f625505eac512270cf3ccc14911
MarcioSebastiao <marcio.sx@outlook.com>
2021-01-04 11:58:29 -0300

    Configuracao api de catalogo - Cors e Swagger adicionados

---
+++ src/service/NSE.Catalogo/Configuration/DependencyInjectionConfig.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using NSE.Catalogo.Data;
+using NSE.Catalogo.Data.Repository;
+using NSE.Catalogo.Models;
+
+namespace NSE.Catalogo.Configuration
+{
+    public static class DependencyInjectionConfig
+    {
+        public static void RegisterServices(this IServiceCollection services)
+        {
+            services.AddScoped<IProdutoRepository, ProdutoRepository>();
+            services.AddScoped<CatalogoContext>();
+        }
+    }
+}
---
+++ src/service/NSE.Catalogo/Configuration/SwaggerConfig.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.OpenApi.Models;
+
+namespace NSE.Catalogo.Configuration
+{
+    public static class SwaggerConfig
+    {
+        public static IServiceCollection AddSwaggerConfiguration(this IServiceCollection services)
+        {
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Title = "NerdStore Enterprise Catalogo API"
+                });
+            });
+
+            return services;
+        }
+
+        public static IApplicationBuilder UseSwaggerConfiguration(this IApplicationBuilder app)
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
+            });
+
+            return app;
+        }
+    }
+}
