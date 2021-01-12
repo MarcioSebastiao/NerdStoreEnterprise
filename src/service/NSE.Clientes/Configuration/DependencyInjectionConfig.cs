e1495f8cfb77ddf8f0f72da6895fba0e68317adf
MarcioSebastiao <marcio.sx@outlook.com>
2021-01-12 15:51:50 -0300

    Repositorio de Clientes criado

---
+++ src/service/NSE.Clientes/Configuration/DependencyInjectionConfig.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using NSE.Clientes.Data;
+using NSE.Clientes.Data.Repository;
+using NSE.Clientes.Models;
+
+namespace NSE.Clientes.Configuration
+{
+    public static class DependencyInjectionConfig
+    {
+        public static void RegisterServices(this IServiceCollection services)
+        {
+            services.AddScoped<IClienteRepository, ClienteRepository>();
+            services.AddScoped<ClientesContext>();
+        }
+    }
+}
