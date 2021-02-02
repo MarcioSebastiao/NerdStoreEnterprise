e43d33f0efe6b2a9b32348af587949a94b829f36
MarcioSebastiao <marcio.sx@outlook.com>
2021-02-02 15:02:14 -0300

    Configuracao da api de Pedidos abstraida

--- src/services/NSE.Pedidos/Startup.cs
+++ src/services/NSE.Pedidos/Startup.cs
@@ -2,7 +2,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
+using NSE.Pedidos.Configuration;
 
 namespace NSE.Pedidos
 {
@@ -17,26 +17,13 @@ namespace NSE.Pedidos
 
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllers();
+            services.AddApiConfiguration();
         }
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-
-            app.UseHttpsRedirection();
-
-            app.UseRouting();
-
-            app.UseAuthorization();
-
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapControllers();
-            });
+            app.UseApiConfiguration(env);
+           
         }
     }
 }
