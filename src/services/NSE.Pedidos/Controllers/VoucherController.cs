a7fa8808f633d35ddd83060a005866365b069ef0
MarcioSebastiao <marcio.sx@outlook.com>
2021-02-05 17:50:09 -0300

    VoucherController adicionado na api de Pedidos - Possibilidade de obter Voucher por codigo

---
+++ src/services/NSE.Pedidos/Controllers/VoucherController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using NSE.Pedidos.Application.Queries;
+using NSE.Pedidos.DTO.Application;
+using NSE.WebAPI.Core.Controllers;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace NSE.Pedidos.Controllers
+{
+    [Authorize]
+    public class VoucherController : MainController
+    {
+        private readonly IVoucherQueries _voucherQueries;
+
+        public VoucherController(IVoucherQueries voucherQueries)
+        {
+            _voucherQueries = voucherQueries;
+        }
+
+
+        [HttpGet("voucher/{codigo}")]
+        [ProducesResponseType(typeof(VoucherDTO), (int) HttpStatusCode.OK)]
+        [ProducesResponseType((int) HttpStatusCode.NotFound)]
+        public async Task<IActionResult> ObterPorCodigo(string codigo)
+        {
+            if (string.IsNullOrEmpty(codigo)) return NotFound();
+
+            var voucher = await _voucherQueries.ObterVoucherPorCodigo(codigo);
+
+            return voucher == null ? NotFound() : CustomResponse(voucher);
+        }
+    }
+}
+ 
