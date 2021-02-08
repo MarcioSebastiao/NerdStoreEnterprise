2f93e373c55b18ce79ce170473cd22e013f0ec41
MarcioSebastiao <marcio.sx@outlook.com>
2021-02-08 18:20:18 -0300

    Validacao para Voucher - Feature SpecValidation

---
+++ src/building blocks/NSE.Core/Specification/Validation/Rule.cs
@@ -0,0 +1,21 @@
+﻿namespace NSE.Core.Specification.Validation
+{
+    public class Rule<T>
+    {
+        private readonly Specification<T> _specificationSpec;
+
+        public Rule(Specification<T> specificationSpec, string errorMessage)
+        {
+            _specificationSpec = specificationSpec;
+            ErrorMessage = errorMessage;
+        }
+
+        public string ErrorMessage { get; }
+
+        public bool Validate(T obj)
+        {
+            return _specificationSpec.IsSatisfiedBy(obj);
+        }
+
+    }
+}
