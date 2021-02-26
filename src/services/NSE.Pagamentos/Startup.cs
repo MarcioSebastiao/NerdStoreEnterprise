1fe8e0ab9d36280a873054a0b6eb4eb46b60e294
MarcioSebastiao <marcio.sx@outlook.com>
2021-02-26 12:39:39 -0300

    Message bus configurado na api de Pagamentos

--- src/services/NSE.Pagamentos/Startup.cs
+++ src/services/NSE.Pagamentos/Startup.cs
@@ -34,6 +34,7 @@ namespace NSE.Pagamentos
             services.AddJwtConfiguration(Configuration);
             services.AddSwaggerConfiguration();
             services.RegisterServices();
+            services.AddMessageBusConfiguration(Configuration);
         }
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
