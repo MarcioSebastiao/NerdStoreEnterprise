5685c5e6a4aa8c0447b87ce1a777e7b868a0969c
MarcioSebastiao <marcio.sx@outlook.com>
2021-03-04 10:39:18 -0300

    Repositorio de Pagamento - Possibilidade de Adicionar e obter Transacao

--- src/services/NSE.Pagamentos/Data/Repository/PagamentoRepository.cs
+++ src/services/NSE.Pagamentos/Data/Repository/PagamentoRepository.cs
@@ -1,5 +1,10 @@
-﻿using NSE.Core.Data;
+﻿using Microsoft.EntityFrameworkCore;
+using NSE.Core.Data;
 using NSE.Pagamentos.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace NSE.Pagamentos.Data.Repository
 {
@@ -17,6 +22,17 @@ namespace NSE.Pagamentos.Data.Repository
         public void AdicionarPagamento(Pagamento pagamento)
         {
             _context.Pagamentos.Add(pagamento);
+        }
+
+        public void AdicionarTransacao(Transacao transacao)
+        {
+            _context.Transacoes.Add(transacao);
+        }
+
+        public async Task<IEnumerable<Transacao>> ObterTransacoesPorPedidoId(Guid pedidoId)
+        {
+            return await _context.Transacoes.AsNoTracking()
+                .Where(t => t.Pagamento.PedidoId == pedidoId).ToListAsync();
         }
 
         public void Dispose()
--- src/services/NSE.Pagamentos/Models/IPagamentoRepository.cs
+++ src/services/NSE.Pagamentos/Models/IPagamentoRepository.cs
@@ -1,9 +1,15 @@
 ﻿using NSE.Core.Data;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace NSE.Pagamentos.Models
 {
     public interface IPagamentoRepository : IRepository<Pagamento>
     {
-        void AdicionarPagamento(Pagamento pagamento); 
+        void AdicionarPagamento(Pagamento pagamento);
+
+        void AdicionarTransacao(Transacao transacao);
+        Task<IEnumerable<Transacao>> ObterTransacoesPorPedidoId(Guid pedidoId);
     }
 }
